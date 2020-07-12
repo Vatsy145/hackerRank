f2231891c5132f236411a5b5863a498bff223d4c
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-07-12 15:01:20 +0300

    solving new problem

---
+++ 10_days_of_JS/day_6_bitwise_operators.js
@@ -0,0 +1,11 @@
+`use strict`
+
+function getMaxLessThanK(n, k){
+    console.log(n)
+    console.log(k)
+}
+
+console.log(getMaxLessThanK(5,2))
+// console.log(getMaxLessThanK(8,5))
+// console.log(getMaxLessThanK(2,2))
+
