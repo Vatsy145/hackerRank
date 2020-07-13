91cb2acfc93fa9004ed1c545b8649a7f3dcc6da5
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-07-13 12:50:29 +0300

    solving new problem

---
+++ 10_days_of_JS/day_7_regular_expressions_I.js
@@ -0,0 +1,24 @@
+`use strict`
+
+function regexVar() {
+    //  ^ => first item matches:
+    // () => stores matching value captured within
+    // [aeiou] => matches any of the characters in the brackets
+    // . => matches any character:
+    // + => for 1 or more occurrances (this ensures str length > 3)
+    // \1 => matches to previously stored match.
+        // \2 looks for matched item stored 2 instances ago
+        // \3 looks for matched item stored 3 ago, etc
+
+    //  $ ensures that matched item is at end of the sequence
+
+    let re = /^([aeiou]).+\1$/;
+    return re;
+}
+
+let re = regexVar()
+
+console.log(re.test(`bcd`));
+console.log(re.test(`abcd`));
+console.log(re.test(`abcda`));
+console.log(re.test(`abcdo`));
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hackerRank
+
+ problems un-solved yet:
+
+### 10_days_of_JS
+* [day_6_bitwise_operators.js](https://github.com/Ahmad-Sawalqeh/hackerRank/blob/master/10_days_of_JS/day_6_bitwise_operators.js)
+
