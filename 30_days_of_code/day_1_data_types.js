92d4e026de9de5ffc5e1b767f4021b4e11c5ac5d
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-07-21 11:45:05 +0300

    solving new problem

---
+++ 30_days_of_code/day_1_data_types.js
@@ -0,0 +1,16 @@
+`use strict`
+
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    let integer, double, string;
+    integer = Number(input_stdin_array[0]);
+    double = Number(input_stdin_array[1]);
+    string = input_stdin_array[2];
+    console.log(i + integer)
+    console.log((d + double).toFixed(1))
+    console.log(`${s}${string}`)
+  }
+  
+  main()
